e1c0e20d8e816495dbf26693cf188605de01e770
pollo <pollo@MacBook-Pro-de-pollo.local>
2011-01-14 14:45:24 +0100

    Création de la vue principale

---
+++ grails-app/controllers/icescrum/plugin/planning/poker/PlanningPokerController.groovy
@@ -0,0 +1,12 @@
+
+class PlanningPokerController {
+
+   static final id = 'planningPoker'
+    static ui = true
+    static menuBar = [show:[visible:true,pos:0],title:'is.ui.planningPoker']
+    static window =  [title:'is.ui.planningPoker',help:'is.ui.planningPoker.help',toolbar:false]
+    def index = {
+       plugin:'iceScrum-plugin-planning-poker'
+
+    }
+}
