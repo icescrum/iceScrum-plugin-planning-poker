741c6eb6934430a921f249c89c5b8df6d402823b
pollo <pollo@MacBook-Pro-de-pollo.local>
2011-01-19 14:35:30 +0100

    Création de la vue principale

--- grails-app/controllers/icescrum/plugin/planning/poker/PlanningPokerController.groovy
+++ grails-app/controllers/icescrum/plugin/planning/poker/PlanningPokerController.groovy
@@ -4,9 +4,8 @@ class PlanningPokerController {
    static final id = 'planningPoker'
     static ui = true
     static menuBar = [show:[visible:true,pos:0],title:'is.ui.planningPoker']
-    static window =  [title:'is.ui.planningPoker',help:'is.ui.planningPoker.help',toolbar:false]
+    static window =  [title:'is.ui.planningPoker', help:'is.ui.planningPoker.help',toolbar:false]
     def index = {
-       plugin:'iceScrum-plugin-planning-poker'
-
+              plugin:'iceScrum-plugin-planning-poker'
     }
 }
--- grails-app/views/planningPoker/index.gsp
+++ grails-app/views/planningPoker/index.gsp
@@ -1,13 +1,13 @@
 <%--
   Created by IntelliJ IDEA.
   User: pollo
-  Date: 14/01/11
-  Time: 14:10
+  Date: 17/01/11
+  Time: 20:45
   To change this template use File | Settings | File Templates.
 --%>
 
 <%@ page contentType="text/html;charset=UTF-8" %>
 <html>
-  <head><title>Simple GSP page</title></head>
-  <body>Place your content here</body>
+  <head><title>Simple INDEX GSP page</title></head>
+  <body>Place your tetetetet</body>
 </html>
