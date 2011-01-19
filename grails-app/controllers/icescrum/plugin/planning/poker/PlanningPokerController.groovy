9bab36e6342addbd6443034ebc042cacaa8737f7
Ju <emotik@MacBook-de-Julien-Goudeaux.local>
2011-01-19 14:56:38 +0100

    Nouvelle tentative de réparation

--- grails-app/controllers/icescrum/plugin/planning/poker/PlanningPokerController.groovy
+++ grails-app/controllers/icescrum/plugin/planning/poker/PlanningPokerController.groovy
@@ -1,11 +1,12 @@
+package icescrum.plugin.planning.poker
 
 class PlanningPokerController {
 
-   static final id = 'planningPoker'
-    static ui = true
-    static menuBar = [show:[visible:true,pos:0],title:'is.ui.planningPoker']
-    static window =  [title:'is.ui.planningPoker', help:'is.ui.planningPoker.help',toolbar:false]
-    def index = {
-              plugin:'iceScrum-plugin-planning-poker'
-    }
+  static final id = 'planningPoker'
+  static ui = true
+  static menuBar = [show:[visible:true,pos:0],title:'is.ui.planningPoker']
+  static window = [title:'is.ui.planningPoker',help:'is.ui.planningPoker.help',toolbar:false]
+  def index = {
+    plugin:'iceScrum-plugin-planning-poker'
+  }
 }
