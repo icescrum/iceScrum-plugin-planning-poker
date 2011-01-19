03a1b884240b6dc879549941a2b1a67580f62120
Ju <emotik@MacBook-de-Julien-Goudeaux.local>
2011-01-19 17:19:16 +0100

    Début ajout toolbar.

--- grails-app/controllers/icescrum/plugin/planning/poker/PlanningPokerController.groovy
+++ grails-app/controllers/icescrum/plugin/planning/poker/PlanningPokerController.groovy
@@ -5,8 +5,10 @@ class PlanningPokerController {
   static final id = 'planningPoker'
   static ui = true
   static menuBar = [show:[visible:true,pos:0],title:'is.ui.planningPoker']
-  static window = [title:'is.ui.planningPoker',help:'is.ui.planningPoker.help',toolbar:false]
+  static window = [title:'is.ui.planningPoker',help:'is.ui.planningPoker.help',init:'index',toolbar:true]
   def index = {
     plugin:'iceScrum-plugin-planning-poker'
+
+    render(template:'window/blank',plugin:'iceScrum-plugin-planning-poker' ,model:[id:id])
   }
 }
---
+++ grails-app/views/planningPoker/window/_blank.gsp
@@ -0,0 +1,8 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: emotik
+  Date: 19/01/11
+  Time: 16:40
+  To change this template use File | Settings | File Templates.
+--%>
+toto
