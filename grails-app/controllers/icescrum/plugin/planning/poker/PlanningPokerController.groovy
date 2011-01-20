757649347c33d79947af42f4bbc19a924983d176
pollo <pollo@MacBook-Pro-de-pollo.local>
2011-01-21 00:27:01 +0100

    Création de la vue principale

--- grails-app/controllers/icescrum/plugin/planning/poker/PlanningPokerController.groovy
+++ grails-app/controllers/icescrum/plugin/planning/poker/PlanningPokerController.groovy
@@ -5,30 +5,37 @@ import org.icescrum.core.domain.Task
 import org.icescrum.core.domain.Product
 import grails.converters.JSON
 import org.icescrum.core.domain.Team
+import org.icescrum.core.domain.PlanningPokerGame
 import org.icescrum.plugins.chat.ChatConnection
 import org.icescrum.plugins.chat.ChatUtils
 import org.icescrum.core.support.ApplicationSupport
 
+import org.springframework.security.core.context.SecurityContextHolder as SCH
 
 
 class PlanningPokerController {
 
   static final id = 'planningPoker'
   static ui = true
+   def springSecurityService
   static menuBar = [show:[visible:true,pos:0],title:'is.ui.planningPoker']
   static window = [title:'is.ui.planningPoker',help:'is.ui.planningPoker.help',toolbar:false]
   def index = {
     plugin:'iceScrum-plugin-planning-poker'
 
+
     def projectMembers = []
-    def users = []
-    Product project = (Product) Product.findById(1)
-   projectMembers = project.getAllUsers()
-    //users: project.getAllUsers()
-
-
+    Product project = (Product) Product.findById(params.product)
+    projectMembers = project.getAllUsers()
+    def suite = []
+    if(project. planningPokerGameType == PlanningPokerGame.FIBO_SUITE)
+         suite= PlanningPokerGame.getInteger(PlanningPokerGame.FIBO_SUITE)
+    else
+         suite= PlanningPokerGame.getInteger(PlanningPokerGame.INTEGER_SUITE)
     render(template:'window/blank',plugin:'iceScrum-plugin-planning-poker' ,model:[
             u:projectMembers,
+            me: User.get(springSecurityService.principal.id),
+            suite_fibo:suite,
             id:id,])
   }
 
--- grails-app/views/planningPoker/window/_blank.gsp
+++ grails-app/views/planningPoker/window/_blank.gsp
@@ -5,14 +5,26 @@
   Time: 16:40
   To change this template use File | Settings | File Templates.
 --%>
-<r:resourceLink url="[dir:'css',file:'planningPoker.css',plugin:'iceScrum-plugin-planning-poker']"  media="screen, projection"/>
-
 
 <ul id="planning-poker-members-list">
   <g:each in="${u}" var="user">
-    <li>
+    <li style="display:inline-block;list-style:none;padding:0 5px 0 5px; text-align:center">
         <is:avatar userid="${user.id}" class="ico"/><br/>
         ${user.firstName} ${user.lastName}
+      <div class="planning-poker-carte ui-corner-all" style="width:50%; margin:auto; border:1px silver solid; height:50px"></div>
+    </li>
+  </g:each>
+</ul>
+
+<div id="planning-poker-table" class="ui-corner-all" style="background-color:#2e8b57; width:90%; height:100px; margin:auto; border:3px #daa520 solid"></div>
+<div align="center"> <is:avatar userid="${me.id}" class="ico"/><br/>
+        ${me.firstName} ${me.lastName}</div>
+
+
+<ul id="planning-poker-card-list">
+  <g:each in="${suite_fibo}" var="n">
+    <li style="display:inline-block;list-style:none;padding:0 5px 0 5px; text-align:center">
+      <div class="planning-poker-carte ui-corner-all" style="width:30px; margin:auto; border:1px silver solid; height:50px">${n}</div>
     </li>
   </g:each>
 </ul>
