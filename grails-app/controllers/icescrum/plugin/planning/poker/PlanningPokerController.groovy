0e38e5b0afe98d0661d08b7c01ea864339cc310b
pollo <pollo@MacBook-Pro-de-pollo.local>
2011-01-20 14:56:05 +0100

    Création de la vue principale

--- grails-app/controllers/icescrum/plugin/planning/poker/PlanningPokerController.groovy
+++ grails-app/controllers/icescrum/plugin/planning/poker/PlanningPokerController.groovy
@@ -1,15 +1,35 @@
-package icescrum.plugin.planning.poker
+import org.icescrum.components.UtilsWebComponents
+import org.icescrum.core.domain.User
+import org.icescrum.core.domain.Task
+
+import org.icescrum.core.domain.Product
+import grails.converters.JSON
+import org.icescrum.core.domain.Team
+import org.icescrum.plugins.chat.ChatConnection
+import org.icescrum.plugins.chat.ChatUtils
+import org.icescrum.core.support.ApplicationSupport
+
+
 
 class PlanningPokerController {
 
   static final id = 'planningPoker'
   static ui = true
   static menuBar = [show:[visible:true,pos:0],title:'is.ui.planningPoker']
-  static window = [title:'is.ui.planningPoker',help:'is.ui.planningPoker.help',init:'index',toolbar:true]
+  static window = [title:'is.ui.planningPoker',help:'is.ui.planningPoker.help',toolbar:false]
   def index = {
     plugin:'iceScrum-plugin-planning-poker'
 
-    render(template:'window/blank',plugin:'iceScrum-plugin-planning-poker' ,model:[id:id])
+    def projectMembers = []
+    def users = []
+    Product project = (Product) Product.findById(1)
+   projectMembers = project.getAllUsers()
+    //users: project.getAllUsers()
+
+
+    render(template:'window/blank',plugin:'iceScrum-plugin-planning-poker' ,model:[
+            u:projectMembers,
+            id:id,])
   }
 
   def pushTest = {
--- grails-app/views/planningPoker/window/_blank.gsp
+++ grails-app/views/planningPoker/window/_blank.gsp
@@ -5,4 +5,14 @@
   Time: 16:40
   To change this template use File | Settings | File Templates.
 --%>
-toto
+<r:resourceLink url="[dir:'css',file:'planningPoker.css',plugin:'iceScrum-plugin-planning-poker']"  media="screen, projection"/>
+
+
+<ul id="planning-poker-members-list">
+  <g:each in="${u}" var="user">
+    <li>
+        <is:avatar userid="${user.id}" class="ico"/><br/>
+        ${user.firstName} ${user.lastName}
+    </li>
+  </g:each>
+</ul>
