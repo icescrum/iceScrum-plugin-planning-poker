3c4f48e47c4c7822c2faa6c9902d1f1df3f0ab25
Vincent Barrier <vincent.barrier@icescrum.com>
2011-01-31 17:01:27 +0100

    fixes

--- grails-app/conf/PlanningPokerResources.groovy
+++ grails-app/conf/PlanningPokerResources.groovy
@@ -2,6 +2,6 @@ modules = {
   'planningPoker' {
       dependsOn 'icescrum'
       resource url: [dir: "css", file: 'planningPoker.css', plugin:'icescrum-plugin-planning-poker'], attrs: [media: 'screen,projection'], bundle:'icescrum'
-      resource url: [dir: 'js/jquery', file: 'jquery.icescrum.planningpoker.js', plugin:'icescrum-plugin-planning-poke'],disposition: 'head', bundle:'icescrum'
+      resource url: [dir: 'js/jquery', file: 'jquery.icescrum.planningpoker.js', plugin:'icescrum-plugin-planning-poker'],disposition: 'head', bundle:'icescrum'
   }
 }
